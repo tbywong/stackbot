8397a9feac3bfa2f84587b46a98612b0f2a294b4
Tiffany Wong <tbywong@gmail.com>
2017-11-19 16:48:54 -0800

    Updated with official message

--- src/onboard.js
+++ src/onboard.js
@@ -13,14 +13,18 @@ const message = {
   token: process.env.SLACK_TOKEN,
   as_user: true,
   link_names: true,
-  text: 'Welcome to the 401[stac]Ks Slack Org!',
+  text: 'Hello! Welcome to the Stacks-Slack, also known as "401stacKs", a resource and space for Main Stacks graduates to discuss work, career opportunities, and overall life tips. We may have finished dancing together, but we’re not done yet.',
   attachments: JSON.stringify([
     {
-      title: 'Rules and Conventions',
-      text: '1. Add your MS level to your display name\n 2. Add your professional title to your profile \n3. Join any channel and contribute!',
+      title: 'HOUSE RULES:',
+      text: '1) Must be out of college minimum 1 year. \n2) Must follow naming convention for display name: “First Last [Lv#]” \n3)Must add professional title to your profile.',
+      color: '#7CB6D1'
+    }, {
+      title: 'HOW TO GET STARTED:',
+      text: '1) Click on “Channels” to the left side bar. \n2) Scroll through, join any channel that excites you. \n3) If there isn’t a space that you are looking for, create it yourself! (Please create a “Private” channel for a space that does not apply to the larger MS group.) \n4) Jump in, ask questions, give tips, send gifs, connect, connect, connect. :heart:',
       color: '#7CD197'
     }
-  ]),
+  ])
 };
 
 const initialMessage = (teamId, userId) => {
